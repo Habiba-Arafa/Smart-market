f91cc6136a9108fc70cce08d2d3127f7fbdae7d4
NourhanDeifSayed <s-nourhan.sayed@zewailcity.edu.eg>
2024-12-22 04:32:42 -0800

    updates

--- myshop/myvenv/Scripts/activate.bat
+++ myshop/myvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\myshop\myvenv
+set VIRTUAL_ENV=D:\new_folder\Smart-market\myshop\myvenv
 
 if not defined PROMPT set PROMPT=$P$G
 
